8c6d215281c1a55d697b3e917036d5b9fcccce5c
Aleksandr Istomin <is.as.001@gmail.com>
2024-06-20 15:02:42 +0700

    fix: update categories to unique name

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_19_084744) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_074729) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
-categories = ['Ruby', 'Ruby on Rails', 'JavaScript', 'DevOps', 'Тестирование', 'Новости и объявления', 'ИТ справочник']
-categories.each { |category| Category.create(name: category) }
+# add categories
+categories = [
+  'Ruby',
+  'Ruby on Rails',
+  'JavaScript',
+  'DevOps',
+  'Тестирование',
+  'Новости и объявления',
+  'ИТ справочник'
+]
+categories.each do |category|
+  Category.create(name: category) unless Category.find_by(name: category)
+end
 
-encrypted = Devise::Encryptor.digest(User, 'B?J1r+@,P%f7aBLzK$^')
-User.create(email: 'as@example.com', encrypted_password: encrypted)
+# add user
+user = {
+  encrypted_password: Devise::Encryptor.digest(User, 'B?J1r+@,P%f7aBLzK$^'),
+  email: 'as@example.com'
+}
+unless User.find_by(email: user[:email])
+  User.create(user)
+end

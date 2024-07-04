27fd3c06e6d32c9d952f40c2874a71f0e7f2dc1c
Aleksandr Istomin <is.as.001@gmail.com>
2024-07-04 12:33:20 +0700

    fix: multiple application.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
-# Pin npm packages by running ./bin/importmap
+# frozen_string_literal: true
 
-pin "application"
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin 'application'
+pin '@rails/ujs', to: '@rails--ujs.js' # @7.1.3
